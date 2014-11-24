5d0785f18a83d1642380a8381b971771d3c3e774
Robciuc Ioan <robciucioan94@gmail.com>
2014-11-24 19:10:26 +0200

    Issue #6 fixed

--- includes/customizer.php
+++ includes/customizer.php
@@ -8,6 +8,28 @@ function medica_lite_sanitize_callback_text( $input ) {
 }
 
 /**
+ *	Sanitize Callback: Text Two
+ */
+function medica_lite_sanitize_callback_text_two( $input ) {
+	if ( $input ) {
+		return wp_kses_post( force_balance_tags( $input ) );
+	} else {
+		return wp_kses_post( force_balance_tags( 'Edit this content from Customizer.' ) );
+	}
+}
+
+/**
+ *	Sanitize Callback: Text Three
+ */
+function medica_lite_sanitize_callback_text_three( $input ) {
+	if ( $input ) {
+		return wp_kses_post( force_balance_tags( $input ) );
+	} else {
+		return wp_kses_post( force_balance_tags( 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.' ) );
+	}
+}
+
+/**
  *	Sanitize Callback: Textarea
  */
 function medica_lite_sanitize_callback_textarea( $input ) {
@@ -210,7 +232,7 @@ function medica_lite_customizer( $wp_customize ) {
 	) );
 
 		/* Article - Title */
-		$wp_customize->add_setting( 'ti_features_article_title', array( 'sanitize_callback' => 'medica_lite_sanitize_callback_text' ) );
+		$wp_customize->add_setting( 'ti_features_article_title', array( 'sanitize_callback' => 'medica_lite_sanitize_callback_text_two' ) );
 		$wp_customize->add_control( 'ti_features_article_title', array(
 		    'label'    => __( 'Articles - Title:', 'ti' ),
 		    'section'  => 'features_settings',
@@ -219,7 +241,7 @@ function medica_lite_customizer( $wp_customize ) {
 		) );
 
 		/* Article - Content */
-		$wp_customize->add_setting( 'ti_features_article_content', array( 'sanitize_callback' => 'medica_lite_sanitize_callback_text' ) );
+		$wp_customize->add_setting( 'ti_features_article_content', array( 'sanitize_callback' => 'medica_lite_sanitize_callback_text_three' ) );
 		$wp_customize->add_control( new Example_Customize_Textarea_Control( $wp_customize, 'ti_features_article_content', array(
 		            'label' 	=> __( 'Article - Content:', 'ti' ),
 		            'section' 	=> 'features_settings',
